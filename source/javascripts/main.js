58b2e095987a427767774f54bdade077f0b22ff7
NikolaevAV <nuriko@inbox.ru>
2014-05-31 16:54:29 +0400

    styled modal form

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -9,4 +9,24 @@ jQuery(document).ready(function(){
   var daynum = today.getDOY();
 
   $('#j-counter').html(daynum * 4);
+
+  $('a.smooth').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
+  $(document).on("scroll", function() {
+    if ( $(document).scrollTop() > $(".clear").offset().top + $(".clear").outerHeight() ) {
+      $("#menu").addClass("menu-fixed");
+    } else {
+      $("#menu").removeClass("menu-fixed");
+    }
+  });
 });
