d48fa3506ceaf93eb99753c9651de369a3b3cb77
NikolaevAV <nuriko@inbox.ru>
2014-06-09 23:00:42 +0400

    fix currency parser

--- source/currency.php
+++ source/currency.php
@@ -3,7 +3,8 @@ $xmlURL = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req='.date('d/m/Y');
 $tpl = isset($tpl) ? $modx->getChunk($tpl) : 'EUR: [+euro+] руб | USD: [+usd+] руб';
 
 //кэширование 6 часов
-$filename = MODX_BASE_PATH.'cache/kurs.xml';
+// $filename = MODX_BASE_PATH.'cache/kurs.xml';
+$filename = 'cache/kurs.xml';
 if (!file_exists($filename) || (file_exists($filename) && time() > filemtime($filename) + 60*60*6)) {
         $file = file_get_contents($xmlURL);
         if ($file) {
@@ -19,6 +20,10 @@ if ($xml != false){
         $euro = $xml->xpath('Valute[@ID="R01239"]/Value');
         $euro = round(str_replace(',','.',$euro[0]),1);
         $output = str_replace(array('[+usd+]','[+euro+]'),array($usd,$euro),$tpl);
-        return $output;
+        // return $output;
+        echo $output;
+} else {
+  echo 'none';
 }
+
 ?>
